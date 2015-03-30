00a4adccea9c61fcdc3bbfb58f09e6bc130ab26c
Andrew Madsen <andrew@mixedinkey.com>
2015-03-29 19:21:18 -0600

    Initial Commit.

---
+++ Sources/ORSSpotsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ORSSpotsTableViewController.h
+//  Spots
+//
+//  Created by Andrew Madsen on 3/29/15.
+//  Copyright (c) 2015 Open Reel Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ORSSpotsTableViewController : UITableViewController
+
+@end
---
+++ Sources/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Spots
+//
+//  Created by Andrew Madsen on 3/29/15.
+//  Copyright (c) 2015 Open Reel Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ORSSpotsAppController.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([ORSSpotsAppController class]));
+	}
+}
